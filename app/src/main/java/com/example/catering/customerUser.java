74fd1f7a0d16777fb9ac4cbf9dc201d925f48164
RayZai <61789884+RayZai@users.noreply.github.com>
2020-11-04 22:19:26 +0800

    Add files via upload

--- app/src/main/java/com/example/catering/customerUser.java
+++ app/src/main/java/com/example/catering/customerUser.java
@@ -3,11 +3,11 @@ package com.example.catering;
 import java.util.ArrayList;
 
 public class customerUser {
-    private String name,id;
-    private boolean membership;
+    private String name,id,dob;
+    private boolean membership,coupon;
     private ArrayList<order> orderHistory;
 
-    public customerUser(String name,String id) {
+    public customerUser(String name,String id,String dob) {
         this.name = name;
         this.membership=false;
         ArrayList<service> tempoList=new ArrayList<>();
@@ -17,6 +17,15 @@ public class customerUser {
         this.id=id;
         this.orderHistory=new ArrayList<>();
         orderHistory.add(sampleSave);
+        this.dob=dob;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
     }
 
     //get name
@@ -48,7 +57,7 @@ public class customerUser {
     public void setOrderHistory(ArrayList<order> orderHistory) {
         this.orderHistory = orderHistory;
     }
-    
+
     //add order
     public void addOrder(order order){
         this.orderHistory.add(order);
@@ -62,5 +71,13 @@ public class customerUser {
     //set id
     public void setId(String id) {
         this.id = id;
+    }
+
+    public boolean isCoupon() {
+        return coupon;
+    }
+
+    public void setCoupon(boolean coupon) {
+        this.coupon = coupon;
     }
 }
--- app/src/main/java/com/example/catering/signUp.java
+++ app/src/main/java/com/example/catering/signUp.java
@@ -1,17 +1,20 @@
 package com.example.catering;
 
+import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -20,9 +23,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Calendar;
+
 public class signUp extends Fragment {
     private EditText confirmPass,password,email,name;
-    private TextView login;
+    private TextView login,dob;
     private onFragmentListener fragmentListener;
     private FirebaseAuth firebaseAuth;
     private ProgressDialog dialog;
@@ -46,11 +51,29 @@ public class signUp extends Fragment {
         email = v.findViewById(R.id.email);
         name = v.findViewById(R.id.name);
         login = v.findViewById(R.id.signUp);
+        dob=v.findViewById(R.id.dob);
+        dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragment=getActivity().getSupportFragmentManager();
+                calenderFrag tempoDate = new calenderFrag();
+                Calendar calender = Calendar.getInstance();
+                Bundle bundle = new Bundle();
+                bundle.putInt("year", calender.get(Calendar.YEAR));
+                bundle.putInt("month", calender.get(Calendar.MONTH));
+                bundle.putInt("day", calender.get(Calendar.DAY_OF_MONTH));
+                tempoDate.setArguments(bundle);
+                tempoDate.setCallBack(ondate,false);
+                if(getFragmentManager()!=null){
+                    tempoDate.show(fragment, "Date Picker");
+                }
+            }
+        });
         databaseReference= FirebaseDatabase.getInstance().getReference();
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!password.getText().toString().matches("") && !email.getText().toString().matches("") && !confirmPass.getText().toString().matches("") && !name.getText().toString().matches("")){
+                if(!password.getText().toString().matches("") && !email.getText().toString().matches("") && !confirmPass.getText().toString().matches("") && !name.getText().toString().matches("")&&!dob.getText().toString().matches("")){
                     if(password.getText().toString().length()>=6){
                         if(password.getText().toString().trim().equals(confirmPass.getText().toString().trim())){
                             dialog.setMessage("Signing up user");
@@ -63,7 +86,7 @@ public class signUp extends Fragment {
                                                 fragmentListener.registerAndLogin(password.getText().toString().trim());
                                                 firebaseAuth.signInWithEmailAndPassword(email.getText().toString().trim(),password.getText().toString().trim());
                                                 Toast.makeText(getContext(), "Sign Up successful", Toast.LENGTH_SHORT).show();
-                                                customerUser user=new customerUser(name.getText().toString().trim(),firebaseAuth.getCurrentUser().getUid());
+                                                customerUser user=new customerUser(name.getText().toString().trim(),firebaseAuth.getCurrentUser().getUid(),dob.getText().toString().trim());
                                                 databaseReference.child("user").child(firebaseAuth.getCurrentUser().getUid()).setValue(user);
                                                 dialog.dismiss();
                                             }
@@ -102,5 +125,18 @@ public class signUp extends Fragment {
         }
 
     }
+
+    private DatePickerDialog.OnDateSetListener ondate = new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            Calendar c;
+            c=Calendar.getInstance();
+            c.set(Calendar.YEAR,year);
+            c.set(Calendar.MONTH,month);
+            c.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+            String[] monthName = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+            dob.setText("Date: "+String.valueOf(dayOfMonth)+" "+monthName[month]+" "+String.valueOf(year));
+
+        }
+    };
 
 }
