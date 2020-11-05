9c6472589a9b788867edb18d108bb4eafa595baf
brandtyc <62086784+brandtyc@users.noreply.github.com>
2020-11-05 23:17:46 +0800

    Update book.java
    Added 'import com.google.firebase.database.annotations.Nullable;'

--- app/src/main/java/com/example/catering/book.java
+++ app/src/main/java/com/example/catering/book.java
@@ -24,6 +24,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Calendar;
